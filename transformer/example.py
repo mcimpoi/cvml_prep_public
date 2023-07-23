Fixed bug in masking (unsqueeze)

transformer/example.py
@@ -57,6 +57,23 @@ def generate_synthetic_data(vocab_size: int, batch_size: int, n_batches: int):
         yield Batch(src, tgt, 0)
 
 
+# TODO: This is copy paste. To understand what it does.
+def greedy_decode(model, src, src_mask, max_len, start_symbol):
+    memory = model.encode(src, src_mask)
+    ys = torch.zeros(1, 1).fill_(start_symbol).type_as(src.data)
+    for i in range(max_len - 1):
+        out = model.decode(
+            memory, src_mask, ys, subsequent_mask(ys.size(1)).type_as(src.data)
+        )
+        prob = model.generator(out[:, -1])
+        _, next_word = torch.max(prob, dim=1)
+        next_word = next_word.data[0]
+        ys = torch.cat(
+            [ys, torch.zeros(1, 1).type_as(src.data).fill_(next_word)], dim=1
+        )
+    return ys
+
+
 def example_inference(decode_steps: int = 10) -> None:
     model: Transformer = Transformer(
         n_blocks=2,
@@ -94,7 +111,9 @@ def example_inference(decode_steps: int = 10) -> None:
     print(f"Example untrained model prediction: {ys}")
 
 
-def example_simple_model_train():
+def example_simple_model_train(
+    n_epochs: int = 10,
+):
     VOCAB_SIZE = 11
     model: Transformer = Transformer(
         n_blocks=2,
@@ -121,7 +140,8 @@ def example_simple_model_train():
 
     batch_size = 10
 
-    for epoch in range(n_epochs):
+    model.train()
+    for _ in range(n_epochs):
         model.train()
         run_epoch(
             data_iter=generate_synthetic_data(
@@ -136,6 +156,27 @@ def example_simple_model_train():
             mode="train",
             accum_iter=1,
         )
+
+        model.eval()
+        eval_loss, _ = run_epoch(
+            data_iter=generate_synthetic_data(
+                vocab_size=VOCAB_SIZE,
+                batch_size=batch_size,
+                n_batches=5,
+            ),
+            model=model,
+            criterion=nn.CrossEntropyLoss(),
+            optimizer=None,
+            scheduler=None,
+            mode="eval",
+        )
+        logger.info(f"Eval loss: {eval_loss}")
+
+    model.eval()
+    src = torch.LongTensor([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]])
+    max_len = src.shape[1]
+    src_mask = torch.ones(1, 1, max_len)
+    print(greedy_decode(model, src, src_mask, max_len=max_len, start_symbol=0))
 
 
 # TODO: greedy decode
@@ -169,23 +210,23 @@ def test_data_iter(model: Transformer, data_iter=None):
         f"Batch src_mask: {batch.src_mask.shape} Batch tgt_mask: {batch.tgt_mask.shape}"
     )
 
-    # out = model.forward(
-    #     batch.src,
-    #     batch.tgt,
-    #     batch.src_mask,
-    #     batch.tgt_mask,
-    # )
-    # print(f"out shape: {out.shape}")
+    out = model.forward(
+        batch.src,
+        batch.tgt,
+        batch.src_mask,
+        batch.tgt_mask,
+    )
+    print(f"out shape: {out.shape}")
 
 
 if __name__ == "__main__":
     # example_inference()
 
-    model = get_model(11, 11, n_blocks=2, d_model=512)
+    model = get_model(11, 11, n_blocks=2, d_model=512, n_heads=8)
 
     from model_reference import make_model, data_gen
 
-    model_ref = make_model(11, 11, N=2)
+    model_ref = make_model(11, 11, N=2, d_model=512)
 
     data_iter = generate_synthetic_data(11, 2, 5)
     data_iter_ref = data_gen(11, 2, 5)
@@ -194,11 +235,5 @@ if __name__ == "__main__":
     test_data_iter(model_ref, data_iter_ref)
 
     model.train()
-    model_ref.train()
 
-    data_iter = generate_synthetic_data(11, 2, 1)
-    batch = next(data_iter)
-
-    # res = model.forward(batch.src, batch.tgt, batch.src_mask, batch.tgt_mask)
-    res = model.encode(batch.src, batch.src_mask)
-    logger.debug(f"res shape: {res.shape}")
+    example_simple_model_train(n_epochs=20)

transformer/model.py
@@ -66,7 +66,7 @@ class Encoder(nn.Module):
 
     def forward(self, x, src_mask):
         logger.debug(
-            f"x: {x.shape}, src_mask:"
+            f"Enc::forward::\n  x: {x.shape}, src_mask:"
             f" {src_mask.shape if src_mask is not None else None}"
         )
         for layer in self.layers:
@@ -95,7 +95,9 @@ class Decoder(nn.Module):
     def forward(self, x, memory, src_mask, tgt_mask):
         for layer in self.layers:
             x = layer(x, memory, src_mask, tgt_mask)
-        return self.norm(x)
+        out = self.norm(x)
+        logger.debug(f"Decoder::forward:: out: {out.shape}")
+        return out
 
 
 class EncoderLayer(nn.Module):
@@ -122,7 +124,7 @@ class EncoderLayer(nn.Module):
 
     def forward(self, x, mask=None):
         logger.debug(
-            f"EncoderLayer: x: {x.shape}"
+            f"EncoderLayer::forward\n x: {x.shape}"
             + f"mask: {mask.shape if mask is not None else None}"
         )
         x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
@@ -290,6 +292,13 @@ class MultiHeadedAttention(nn.Module):
 
     def forward(self, query, key, value, mask=None):
         # query: (batch_size, n_queries, d_model)
+
+        # this seems important!
+        # same mask is applied to all heads
+
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+
         logger.debug(f"MultiHeadedAttention:: query: {query.shape}")
         Q = (
             self.Wq(query)
@@ -309,7 +318,7 @@ class MultiHeadedAttention(nn.Module):
 
         logger.debug(
             f"Q: {Q.shape}, K: {K.shape}, V: {V.shape}, "
-            + "mask: {mask.shape if mask is not None else None}"
+            + f"mask: {mask.shape if mask is not None else None}"
         )
         x, _ = self.attention(Q, K, V, mask, self.dropout_prob)
 
@@ -319,7 +328,9 @@ class MultiHeadedAttention(nn.Module):
             .view(x.shape[0], -1, self.num_heads * self.d_k)
         )
 
-        return self.Wo(x)
+        out = self.Wo(x)
+        logger.debug(f"out: {out.shape}")
+        return out
 
     def attention(self, query, key, value, mask=None, dropout_prob=None):
         # query: (batch_size, vocab_size
@@ -327,7 +338,9 @@ class MultiHeadedAttention(nn.Module):
         # value: (batch_size, n_keys, d_model)
         # ?? mask: (batch_size, n_queries, n_keys)
         scores = torch.matmul(query, key.transpose(-2, -1)) / self.d_k**0.5
-        logger.debug(f"query: {query.shape}, key: {key.shape}, value: {value.shape}")
+        logger.debug(
+            f"attn::\n query: {query.shape}, key: {key.shape}, value: {value.shape}"
+        )
         logger.debug(f"scores: {scores.shape}")
         logger.debug(f"mask: {mask.shape if mask is not None else None}")
         if mask is not None:

transformer/model_reference.py
@@ -68,7 +68,8 @@ class Encoder(nn.Module):
     def forward(self, x, mask):
         "Pass the input (and mask) through each layer in turn."
         logger.debug(
-            f"x: {x.shape}, src_mask: {mask.shape if mask is not None else None}"
+            f"Enc::forward::\n  x: {x.shape}, src_mask:"
+            f" {mask.shape if mask is not None else None}"
         )
         for layer in self.layers:
             x = layer(x, mask)
@@ -118,10 +119,13 @@ class EncoderLayer(nn.Module):
 
     def forward(self, x, mask):
         "Follow Figure 1 (left) for connections."
-        print("EncoderLayer:: X_in shape", x.shape)
+        logger.debug(
+            f"EncoderLayer::forward\n x: {x.shape}"
+            + f"mask: {mask.shape if mask is not None else None}"
+        )
         x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
         out = self.sublayer[1](x, self.feed_forward)
-        print(f"EncoderLayer:: Out shape {out.shape}")
+        logger.debug(f"EncoderLayer:: Out shape {out.shape}")
         return out
 
 
@@ -136,7 +140,10 @@ class Decoder(nn.Module):
     def forward(self, x, memory, src_mask, tgt_mask):
         for layer in self.layers:
             x = layer(x, memory, src_mask, tgt_mask)
-        return self.norm(x)
+
+        out = self.norm(x)
+        logger.debug(f"Decoder::forward:: out: {out.shape}")
+        return out
 
 
 class DecoderLayer(nn.Module):
@@ -211,7 +218,7 @@ class MultiHeadedAttention(nn.Module):
             for lin, x in zip(self.linears, (query, key, value))
         ]
 
-        print(
+        logger.debug(
             f"""After projection Q: {query.shape}, K: {key.shape},
             value: {value.shape}\n
             """
@@ -223,7 +230,7 @@ class MultiHeadedAttention(nn.Module):
         # 3) "Concat" using a view and apply a final linear.
         x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
 
-        print(f"concat, before last linear {x.shape}")
+        logger.debug(f"concat, before last linear {x.shape}")
         del query
         del key
         del value
@@ -318,7 +325,7 @@ def inference_test():
             [ys, torch.empty(1, 1).type_as(src.data).fill_(next_word)], dim=1
         )
 
-    print("Example Untrained Model Prediction:", ys)
+    logger.info("Example Untrained Model Prediction:", ys)
 
 
 def run_tests():
@@ -396,7 +403,7 @@ def run_epoch(
         if i % 40 == 1 and (mode == "train" or mode == "train+log"):
             lr = optimizer.param_groups[0]["lr"]
             elapsed = time.time() - start
-            print(
+            logger.info(
                 (
                     "Epoch Step: %6d | Accumulation Step: %3d | Loss: %6.2f "
                     + "| Tokens / Sec: %7.1f | Learning Rate: %6.1e"

transformer/training.py
@@ -3,10 +3,11 @@ import logging
 import torch
 import torch.nn as nn
 import torch.utils.data
+import time
 
 from transformer.model import Transformer
 
-from typing import Generator, Tuple
+from typing import Generator, Tuple, Optional
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -24,11 +25,12 @@ def run_epoch(
     data_iter: Generator,
     model: Transformer,
     criterion: nn.Module,
-    optimizer: torch.optim.Optimizer,
-    scheduler: torch.optim.lr_scheduler._LRScheduler,
+    optimizer: Optional[torch.optim.Optimizer],
+    scheduler: Optional[torch.optim.lr_scheduler.LRScheduler],
     mode: str = "train",
     accum_iter: int = 1,
     train_state=TrainState(),  # TODO: do we need it?
+    log_freq: int = 50,
 ) -> Tuple[float, TrainState]:
     """
     Standard Training and Logging Function
@@ -37,6 +39,7 @@ def run_epoch(
     total_loss = 0
     tokens = 0
     n_accum = 0
+    start_time = time.time()
 
     for i, batch in enumerate(data_iter):
         out = model.forward(
@@ -55,15 +58,28 @@ def run_epoch(
             train_state.tokens += batch.n_tokens
 
             if i % accum_iter == 0:
-                optimizer.step()
-                optimizer.zero_grad()  # set_to_none=True by default
-                scheduler.step()
+                if optimizer is not None:
+                    optimizer.step()
+                    optimizer.zero_grad()  # set_to_none=True by default
                 n_accum += 1
                 train_state.accum_step += 1
-            scheduler.step()
+            if scheduler is not None:
+                scheduler.step()
 
         total_loss += loss
         total_tokens += batch.n_tokens
         tokens += batch.n_tokens
+
+        if i % log_freq == 1 and mode.startswith("train"):
+            elapsed = time.time() - start_time
+            if optimizer is None:
+                logger.warning("Optimizer should not be None in train mode")
+            lr = optimizer.param_groups[0]["lr"]
+            logger.info(
+                f"Epoch Step: {i:06d} | Accummulation step {n_accum:03d} | "
+                f"Loss: {loss_node.item():.2f} |"
+                f" Tokens per Sec: {tokens / elapsed:.2f} Lr: {lr:.6f}"
+            )
+            tokens = 0
 
     return total_loss, train_state